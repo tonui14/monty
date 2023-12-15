4e900dd012729c0294abecc10c6d90364c2ee707
Daisy Tonui <tonuidaisy@gmail.com>
2023-12-14 16:24:28 -0800

    main.c

--- main.c
+++ main.c
@@ -1,41 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <monty.h>
+#include "monty.h"
 
 #define STACK_SIZE 4
 
-int main()
+int main(int argc, char *argv[])
 {
-	int selection;
-	while (1)
-	{
-		printf("Carry out operations on stack");
-		printf("\n1.Push\n2.Pop\n3.Show element\n4.End");
-		printf("\n\n Enter selection");
+	FILE *filePointer;
+	stack_t *myStack = NULL;
+	char *currentline = NULL;
+	char *currentOpcode;
+	char *currentArgument;
+	unsigned int lineNumber = 0;
+	size_t lineLength = 0;
+	ssize_t bytesRead = 0;
+	const char *DELIMITERS = " \t\n";
 
-		char input(50);
-		scanf("%s", input);
-		selection = atoi(input);
-		
-		if (selection == 1)
-		{
-			push();
-		}
-		else if (selection == 2)
-		{
-			pop();
-		}
-		else if (selection == 3)
-		{
-			show();
-		}
-		else if (selection == 4)
-		{
-			exit(0);
-		}
-		else
-		{
-			printf("\nInvalid choice!");
-		}
+	if (argc != 2) // Check for the correct number of arguments
+	{
+		fprintf(stderr, "USAGE; my_program file \n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
-}_
+	filePointer = fopen(argv[1], "r"); // Open the file if the argument is 1
+	if (filePointer == NULL)
+	{
+		fprintf(stderr, "Error; Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (bytesRead >= 0)  // Read lines from the file
+	{
+		bytesRead = getline(&currentline, &lineLength, file pointer);
+		lineNumber ++;
+		currentOpcode = strtok(&currentLine, DELIMITERS);
+
+		if (bytesRead >= 0)
+		{
+			execute(currentline, &myStack, lineNumber, filePointer); //there will be an execute function
+			free(currentline);  // Free the memory allocated for currentLine
+		}
+		free_stack(myStack);
+		fclose(filePointer); // Close the file and free allocated memory
+	}
+	return(0);
+}
+
