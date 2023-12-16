45869c6510c6bd81c08bb35eec1251d4f3959c67
Daisy Tonui <tonuidaisy@gmail.com>
2023-12-15 17:18:54 -0800

    push.c

--- push.c
+++ push.c
@@ -9,7 +9,7 @@
 void f_push(stack_t **head, unsigned int lineNumber)
 {
 	int n;
-	// Continue until a valid numeric argument is provided
+	/*Continue until a valid numeric argument is provided*/
 	do
 	{
 		if (!currentArgument || !if_numeric(currentArgument))
@@ -20,10 +20,10 @@ void f_push(stack_t **head, unsigned int lineNumber)
 			free_stack(*head);
 			exit(EXIT_FAILURE);
 		}
-		// Convert currentArgument to an integer
+		/*Convert currentArgument to an integer*/
 		n = atoi(currentArgument);
-		// Add the integer to the stack using the addnode function
-		// If malloc fails, print an error message and exit
+		/*Add the integer to the stack using the addnode function*/
+		/*If malloc fails, print an error message and exit*/
 		if (!addnode(myStack, n))
 		{
 			fprintf(stderr, "Error: malloc failed\n");
@@ -32,7 +32,7 @@ void f_push(stack_t **head, unsigned int lineNumber)
 			free_stack(*myStack);
 			exit(EXIT_FAILURE);
 		}
-		// Get the next line in case currentArgument was not an integer
+		/*Get the next line in case currentArgument was not an integer*/
 		free(currentline);
 		currentline = NULL;
 		if (getline(&currentline, &lineLength, filePointer) == -1)
