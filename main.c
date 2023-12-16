eb4ff18aac0a154dd415de69905554936b3bd025
Daisy Tonui <tonuidaisy@gmail.com>
2023-12-15 16:38:01 -0800

    main.c

--- main.c
+++ main.c
@@ -20,18 +20,18 @@ int main(int argc, char *argv[])
 	ssize_t bytesRead = 0;
 	const char *DELIMITERS = " \t\n";
 
-	if (argc != 2) // Check for the correct number of arguments
+	if (argc != 2) /* Check for the correct number of arguments*/
 	{
-		fprintf(stderr, "USAGE; my_program file \n", argv[0]);
+		fprintf(stderr, "USAGE; monty file\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	filePointer = fopen(argv[1], "r"); // Open the file if the argument is 1
+	filePointer = fopen(argv[1], "r"); /* Open the file if the argument is 1*/
 	if (filePointer == NULL)
 	{
-		fprintf(stderr, "Error; Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (bytesRead >= 0)  // Read lines from the file
+	while (bytesRead >= 0)  /* Read lines from the file*/
 	{
 		bytesRead = getline(&currentline, &lineLength, file pointer);
 		lineNumber ++;
@@ -39,12 +39,12 @@ int main(int argc, char *argv[])
 
 		if (bytesRead >= 0)
 		{
-			execute(currentline, &myStack, lineNumber, filePointer); //there will be an execute function
-			free(currentline);  // Free the memory allocated for currentLine
+			execute(currentline, &myStack, lineNumber, filePointer); /*there will be an execute function*/
+			free(currentline);  /*Free the memory allocated for currentLine*/
 		}
-		free_stack(myStack);
-		fclose(filePointer); // Close the file and free allocated memory
 	}
+	free_stack(myStack);
+	fclose(filePointer); /*Close the file and free allocated memory*/
 	return(0);
 }
 
