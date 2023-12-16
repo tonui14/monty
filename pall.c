8a2b362b551fecc73942bd6e3f3f6d8a6f2b0ac0
Daisy Tonui <tonuidaisy@gmail.com>
2023-12-15 17:05:40 -0800

    pall.c

--- pall.c
+++ pall.c
@@ -11,11 +11,11 @@ void f_pall(stack_t **head, unsigned int lineNumber)
 {
 	stack_t *s;
 
-	//Suppress unused variable
+	/*Suppress unused variable*/
 	(void)lineNumber;
 	s = *head;
 
-	// Iterates through the stack and print each element
+	/*Iterates through the stack and print each element*/
 	while (s != NULL)
 	{
 		printf("%d\n", s->n);
